e79f2ec150b0ae2cd491a7d00a52deab999b0e07
Ludovic Morin <ludovic@abstra208.com>
2026-01-27 12:33:58 -0500

    fix: remove unnecessary HTML rendering for improved content display

--- src/routes/waitlist/+page.svelte
+++ src/routes/waitlist/+page.svelte
@@ -6,10 +6,10 @@
     <h1
         class="text-primary-600 dark:text-primary-400 mb-4 leading-tight font-bold sm:mb-6 sm:text-3xl md:text-4xl lg:text-5xl"
     >
-        {@html m.home_finalCta_button()}
+        {m.home_finalCta_button()}
     </h1>
     <div>
-        <p>{@html m.home_finalCta_description()}</p>
-        <button class="btn rounded-3xl">{@html m.home_finalCta_soon_button()}</button>
+        <p>{m.home_finalCta_description()}</p>
+        <button class="btn rounded-3xl">{m.home_finalCta_soon_button()}</button>
     </div>
 </div>
