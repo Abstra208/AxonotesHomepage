93a60551bc8e93bfeaabb43b50afe73657b61ac4
Ludovic Morin <ludovic@abstra208.com>
2026-01-27 12:35:47 -0500

    fix: update layout structure for improved alignment and responsiveness

--- src/routes/waitlist/+page.svelte
+++ src/routes/waitlist/+page.svelte
@@ -2,14 +2,16 @@
     import * as m from "$lib/paraglide/messages.js";
 </script>
 
-<div class="container w-screen max-w-none flex flex-col justify-center items-center">
-    <h1
-        class="text-primary-600 dark:text-primary-400 mb-4 leading-tight font-bold sm:mb-6 sm:text-3xl md:text-4xl lg:text-5xl"
-    >
-        {m.home_finalCta_button()}
-    </h1>
-    <div>
-        <p>{m.home_finalCta_description()}</p>
-        <button class="btn rounded-3xl">{m.home_finalCta_soon_button()}</button>
+<div class="w-full flex justify-center px-4 sm:px-12 lg:px-25">
+    <div class="w-full max-w-6xl">
+        <h1
+            class="text-primary-600 dark:text-primary-400 mb-4 leading-tight font-bold sm:mb-6 text-3xl md:text-4xl lg:text-5xl"
+        >
+            {m.home_finalCta_button()}
+        </h1>
+        <div>
+            <p>{m.home_finalCta_description()}</p>
+            <button class="btn rounded-3xl">{m.home_finalCta_soon_button()}</button>
+        </div>
     </div>
 </div>
